c7c8e7b35d7cdd9d5364a3303af494918c0d1c97
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-04 09:12:40 +0530

    changes

--- Assignments/Q1.py
+++ Assignments/Q1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.constants as con
 
-h = imput('Enter the height:')
+h = float(input("Enter the height: "))
 
 
 print("Time taken by the ball to hit the ground: %.4f seconds" % np.sqrt(2*h/con.g))
