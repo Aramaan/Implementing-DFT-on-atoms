18d3baa574f7089966f67120ce0800f60d33a031
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-05 07:32:30 +0530

    13

--- Assignments/HW1/Q13.py
+++ Assignments/HW1/Q13.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -6,6 +7,42 @@ from matplotlib import pyplot as plt
 Plotting Experimental Data
 '''
 '''
+
 13(a)
 '''
-data = pd.read_csv(sunspots.txt)
+#%%
+data = pd.read_csv('sunspots.txt',delimiter='\t')
+data.columns = ['Time','Sunspots']
+plt.plot(data['Time'], data['Sunspots'])
+plt.title('Number of Sunspots vs Time')
+plt.xlabel('Time in months')
+plt.ylabel('Number of Sunspots')
+plt.savefig("Q13_a.png")
+plt.show()
+
+'''
+
+13(b)
+'''
+# %%
+plt.plot(data['Time'][0:1000], data['Sunspots'][0:1000])
+plt.title('Number of Sunspots vs Time')
+plt.xlabel('Time in months')
+plt.ylabel('Number of Sunspots')
+plt.savefig("Q13_b.png")
+plt.show()
+
+'''
+
+13(c)
+'''
+# %%
+Sunspots = data['Sunspots'].rolling(window=11).mean()
+plt.plot(data['Time'][0:1000], data['Sunspots'][0:1000])
+plt.plot(data['Time'][0:1000], Sunspots[0:1000])
+plt.title('Number of Sunspots vs Time')
+plt.xlabel('Time in months')
+plt.ylabel('Number of Sunspots')
+plt.savefig("Q13_c.png")
+plt.show()
+# %%
