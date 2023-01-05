789f1e6bdddc93fe549adc5fb7232757c8db0338
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-05 06:46:23 +0530

    h

--- Assignments/HW1/Q13.py
+++ Assignments/HW1/Q13.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 '''
 Plotting Experimental Data
 '''
-
+'''
+13(a)
+'''
+data = pd.read_csv(sunspots.txt)
