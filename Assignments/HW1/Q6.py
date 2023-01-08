9b9da5a7d96ed4f627fde1ce6c7d0a19f5cb5c48
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-09 01:13:50 +0530

    Done

--- Assignments/HW1/Q6.py
+++ Assignments/HW1/Q6.py
@@ -19,8 +19,7 @@ print("Aphelion velocity: %.4f" % v2)
 print("Orbital period: %.4f" % T)
 print("Eccentricity: %.4f" % e)
 
-"""
--------------------------------------------------------------
+'''
 Earth :-
 Perihelion distance - 1.4710e11
 Perihelion velocity- 3.0287e4
@@ -28,12 +27,12 @@ Aphelion distance- 152111350728.5926
 Aphelion velocity- 29289.1864
 Orbital period- 1.0001
 Eccentricity- 0.0167
--------------------------------------------------------------
-For Halley's Comet
+
+Halley's Comet :-
 Perihelion distance- 8.7830e10
 Perihelion velocity- 5.4529e4
 Aphelion distance- 5371566481143.3535
 Aphelion velocity- 891.5988
 Orbital period- 77.9457
 Eccentricity- 0.9678
-"""
+'''
