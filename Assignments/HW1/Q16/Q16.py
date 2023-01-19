1249f9debbfed127bcada9680d92b828394c977b
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-20 01:09:39 +0530

    f

--- Assignments/HW1/Q16/Q16.py
+++ Assignments/HW1/Q16/Q16.py
@@ -4,13 +4,49 @@ from matplotlib import pyplot as plt
 
 # %%
 r = np.arange(0,4,0.01)
+
+'''
+Performs the map operation once on 
+the input value x wih a given r  for rate
+
+input;
+x = input to map
+r = rate constant of map
+
+output;
+ouput of map
+'''
 def map(x,r):
     return r*x*(1-x)
+
+'''
+Repeats the map operation with initial input 0.5
+1000 times for the output values to reach
+ equilibrium (cycle or point)
+
+input;
+r = rate constant of map function
+
+output;
+value of the map after thousandth iteration
+'''
 def iterate(r):
     x = 0.5
     for i in range(1000):
         x = map(x,r)
     return x
+
+'''
+Takes the output from iterate() and 
+stores the next thousand values in an array x
+ and returns it as output
+
+input;
+r = rate constant of map function
+
+output;
+array of 1000 elements, values of output of map after equilibrium
+'''
 def iterate2(r):
     x = []
     x.append(iterate(r))
