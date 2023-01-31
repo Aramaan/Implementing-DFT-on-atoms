7c342b56fc1143fef4506c1d1048de53dc724d0b
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-31 17:02:06 +0530

    x

--- Assignments/HW2/Q2/Q2.py
+++ Assignments/HW2/Q2/Q2.py
@@ -6,32 +6,36 @@ from matplotlib import pyplot as plt
 def f(x):
     return (x**4-2*x+1)
 
-x = np.linspace(0,2,10)
-y = f(x)
 
-def simp(s,j,x):
-    area =(s/2)*(d+c)
-    pass
 
 def sIntegration(x,y):
     integral = [0]
     y0 = y[0]
     y1 = y[1]
     y2 = y[2]
-    s = y1 -y0
+    s = x[1] -x[0]
     for i in range(2,len(x)):
+        
         if i%2 != 0:
             continue
+        
         area = (s)*(y[i-2]+4*y[i-1]+y[i])/3
-        integral.append( integral[i-2] + area)
+        integral.append(integral[i//2-1] + area)
+        print(i/2-1)
+        print(integral)
         y0 = y1
         y1 = y2
         y2 = y[i]
 
     return integral 
 
+def Simpson(a,b,n,f):
+    x = np.linspace(a,b,n)
+    y = f(x)
+
+# %%
 plt.plot(x,y)
 I = sIntegration(x,y)
 x
-plt.plot(x,I)
+plt.plot(x[::2],I)
 # %%
