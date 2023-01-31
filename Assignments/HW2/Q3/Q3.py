e1548f869bdf6b0e8a4ae40947cc3859689f3c09
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-31 17:01:59 +0530

    x

--- Assignments/HW2/Q3/Q3.py
+++ Assignments/HW2/Q3/Q3.py
@@ -1,8 +1,22 @@
+
+#%%
 from matplotlib import pyplot as plt
 import numpy as np
 
 def f(t):
     return np.exp(-t**2)
 
-from Q2.Q2 import simp
+def step(a,b,s,f):
+    x = np.arange(a,b,s)
+    y = f(x)
+    return x,y
 
+from Q2.Q2 import sIntegration
+
+x,y = step(0,3,0.1,f)
+I = sIntegration(x,y)
+plt.plot(x[::2],I)
+plt.show()
+ 
+
+# %%
