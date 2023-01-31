8d6c25f3ef82a45862e911356652dcb94451035a
Aramaan Meher <aramaanmeher@gmail.com>
2023-01-31 21:59:10 +0530

    n

--- Assignments/HW2/Ques4/Q4.py
+++ Assignments/HW2/Ques4/Q4.py
@@ -1,7 +1,9 @@
 #%%
 from matplotlib import pyplot as plt
 import numpy as np
-import Ques2.Q2
+import sys, os
+sys.path.append(os.path.abspath(".."))
+from Ques2.Q2 import sIntegration
 
 # %%
 def J(theta,m,x):
