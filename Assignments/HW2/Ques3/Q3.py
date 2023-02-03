c48e7972d7d4098bb7810746ddac8e1116293f56
Aramaan Meher <aramaanmeher@gmail.com>
2023-02-04 03:09:03 +0530

    j

--- Assignments/HW2/Ques3/Q3.py
+++ Assignments/HW2/Ques3/Q3.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 import sys, os.path
 sys.path.append(os.path.abspath('.'))
-from Ques2.Q2 import simpson
+from Packages.Integration import simpson
 
 def f(t):
     return np.exp(-t**2)
 
-x = np.arange(0,3,0.01)
+x = np.arange(0,3,0.1)
 E = [simpson(0,x[i],i+1,f) for i in range(len(x))]
 plt.plot(x,E)
 plt.show()
---
+++ Assignments/HW2/Ques7/Q7.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sys, os.path
+sys.path.append(os.path.abspath('.'))
+from Ques2.Q2 import simpson
+
+def f(x):
+    return (np.sin(np.sqrt(100*x)))**2
+
+maxE = 10**(-6)
+
+n = 1
+Ii = 
+while(np.abs(If -Ii)> maxE):
+
+    print('Slices: {}, Integral Estimate: {}, Error Estimate: {}'.format())
