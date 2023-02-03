fec0fa90c52623d27f4394f77fe7fa992a032a93
Aramaan Meher <aramaanmeher@gmail.com>
2023-02-04 00:39:02 +0530

    z

--- Assignments/HW2/Ques1/Q1.py
+++ Assignments/HW2/Ques1/Q1.py
@@ -1,4 +1,4 @@
-#%%
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -6,14 +6,14 @@ import sys, os.path
 sys.path.append(os.path.abspath('.'))
 from Packages.Integration import tIntegraion
 
-df = pd.read_csv('velocities.txt',delimiter='\t',names=['time','velocities'])
+df = pd.read_csv('Ques1/velocities.txt',delimiter='\t',names=['time','velocities'])
 t = df['time']
 v = df['velocities']
 
-df
-# %%
 plt.plot(t,v)
+plt.xlabel('time')
 x = tIntegraion(t,v)
-x
 plt.plot(t,x)
-# %%
+plt.legend(['velocity','position'])
+plt.show()
+
