0b0dc42573fc2700031025d0d58d25c250ee9d5d
Aramaan Meher <aramaanmeher@gmail.com>
2023-02-04 01:36:13 +0530

    w

--- Assignments/HW2/Ques3/Q3.py
+++ Assignments/HW2/Ques3/Q3.py
@@ -4,24 +4,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 import sys, os.path
 sys.path.append(os.path.abspath('.'))
-from Ques2.Q2 import sIntegration
+from Ques2.Q2 import simpson
 
 def f(t):
     return np.exp(-t**2)
 
-def step(a,b,s,f):
-    x = np.arange(a,b,s)
-    y = f(x)
-    return x,y
-
-
-
-x,y = step(0,3,0.1,f)
-plt.plot(x,y)
-I = sIntegration(x,y)
-plt.plot(x[::2],I)
+x = np.arange(0,3,0.01)
+E = [simpson(0,x[i],i+1,f) for i in range(len(x))]
+plt.plot(x,E)
 plt.show()
-# print(os.path.abspath('.'))
-# print(Path('Q2.py').resolve())
+
 
 # %%
