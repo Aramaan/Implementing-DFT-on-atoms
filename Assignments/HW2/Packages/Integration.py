bbaf115742da4c47ea8b9cd03b1a66ef41733520
Aramaan Meher <aramaanmeher@gmail.com>
2023-02-04 00:25:59 +0530

    cz

--- Assignments/HW2/Packages/Integration.py
+++ Assignments/HW2/Packages/Integration.py
@@ -2,6 +2,49 @@ from scipy.special import roots_legendre
 import numpy as np
 
 '''
+Does Trapezoidal integration
+ for a given function in certain interval
+'''
+
+def trap(a,b,c,d):
+    area =((b-a)/2)*(d+c)
+    return area
+
+def tIntegraion(x,y):
+    integral = [0]
+    for i in range(1,len(x)):
+        area = trap(x[i-1],x[i],y[i-1],y[i])
+        integral.append( integral[i-1] + area)
+    return integral
+
+'''
+Does Simpson integration
+ for a given function in certain interval
+'''
+def sIntegration(x,y):
+    integral = [0]
+    s = x[1] -x[0]
+    for i in range(2,len(x)):
+        if i%2 != 0:
+            continue
+        area = (s)*(y[i-2]+4*y[i-1]+y[i])/3
+        integral.append(integral[i//2-1] + area)
+    return integral
+
+def Simpson(a,b,n,f):
+    x = np.linspace(a,b,n)
+    y = f(x)
+    I = sIntegration(x,y)
+    return I
+
+'''
+Does Romberg integration
+ for a given function in certain interval
+'''
+def Romberg():
+    pass
+
+'''
 maps the points and weights for 
 integration in any interval [a,b] to the interval
 [-1,1]
@@ -12,7 +55,7 @@ def mapper(x,w,a,b):
     return [x,w]
 
 '''
-Does Gauss-Legendre (Gaussiun) Quadrature interation
+Does Gauss-Legendre (Gaussiun) Quadrature integration
  for a given function in certain interval
 '''
 def GaussQuad(a,b,N,f):
@@ -25,6 +68,7 @@ def GaussQuad2d(a,b,n,f,*z):
     [x,wx] = roots_legendre(n+1)
     [y,wy] = roots_legendre(n+1)
     [x,wx] = mapper(x,wx,a,b)
+    [y,wy] = mapper(y,wy,a,b)
     x,y = np.meshgrid(x,y)
     wx,wy = np.meshgrid(wx,wy)
     integral = np.sum(wx*wy*f(x,y,*z))
--- Assignments/HW2/Ques2/Q2.py
+++ Assignments/HW2/Ques2/Q2.py
@@ -2,41 +2,24 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import sys, os.path
+sys.path.append(os.path.abspath('.'))
+from Packages.Integration import Simpson
 
 def f(x):
     return (x**4-2*x+1)
 
-
-
-def sIntegration(x,y):
-    integral = [0]
-    s = x[1] -x[0]
-    for i in range(2,len(x)):
-        
-        if i%2 != 0:
-            continue
-        
-        area = (s)*(y[i-2]+4*y[i-1]+y[i])/3
-        integral.append(integral[i//2-1] + area)
-        print(i/2-1)
-        print(integral)
-
-    return integral 
-
-def Simpson(a,b,n,f):
-    x = np.linspace(a,b,n)
-    y = f(x)
-    return x,y
-
-
-
-x,y = Simpson(0,2,101,f)
+a = 0
+b = 2
+N = 10
+x = np.linspace(a,b,N)
+y = f(x)
 plt.plot(x,y)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Function and its integral')
 plt.legend(['function','Integral'])
-I = sIntegration(x,y)
+I = Simpson(a,b,N,f)
 print(x.shape)
 plt.plot(x[::2],I)
 plt.show()
